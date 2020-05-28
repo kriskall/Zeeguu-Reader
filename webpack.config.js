03b5d9925be1a19148070f464c88e78fa541d93c
argykay <argykonstantina@gmail.com>
2020-05-28 17:19:29 +0200

    trial

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,58 @@
-const webpack = require('webpack'),
-	path = require('path'),
-	ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack"),
+  path = require("path"),
+  ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var inProduction = process.env.NODE_ENV === 'production';
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+var inProduction = process.env.NODE_ENV === "production";
 
 function getVersion() {
-	return require("./package.json").version;
+  return require("./package.json").version;
 }
 
 module.exports = {
-	mode: 'development',
-	entry: {
-		subscription: './src/umr/static/scripts/app/subscription/main.js',
-		translation: './src/umr/static/scripts/app/translation/main.js'
-	},
-	output: {
-		path: path.join(__dirname, './src/umr/static/scripts/dist'),
-		filename: '[name]-' + getVersion() + '.js'
-	},
-	module: {
-		rules: [{
-			test: /\.css$/,
-			use: ExtractTextPlugin.extract({
-				use: 'css-loader'
-			})
-		},
-		{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader',
-			options: {
-				presets: ['env']
-			}
-		}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('css/[name]-' + getVersion() + '.css'),
-	],
-	stats: {
-		colors: true
-	},
-	devtool: 'source-map'
+  mode: "development",
+  entry: {
+    subscription: "./src/umr/static/scripts/app/subscription/main.js",
+    translation: "./src/umr/static/scripts/app/translation/main.js",
+  },
+  output: {
+    path: path.join(__dirname, "./src/umr/static/scripts/dist"),
+    filename: "[name]-" + getVersion() + ".js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: "css-loader",
+        }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["env"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name]-" + getVersion() + ".css"),
+    new CopyWebpackPlugin([
+      {
+        from: "./src/umr/static/scripts/app/helpers",
+        to: "./src/umr/static/scripts/dist/helpers",
+      },
+    ]),
+  ],
+  stats: {
+    colors: true,
+  },
+  devtool: "source-map",
 };
 
 if (inProduction) {
-	// Do production specific things here.
+  // Do production specific things here.
 }
